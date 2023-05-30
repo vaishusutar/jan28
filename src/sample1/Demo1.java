96a82413479f6f7a463ce97cec9967784e0c8d33
vsuta <vsuta@DELL>
2023-05-30 17:31:52 +0530

    [30 may 23]git hub practice project

---
+++ src/sample1/Demo1.java
@@ -0,0 +1,11 @@
+package sample1;
+
+public class Demo1 
+{
+	public static void main(String[] args) {
+		System.out.println("hii");
+		System.out.println("gm");
+		System.out.println("gn");
+	}
+
+}
